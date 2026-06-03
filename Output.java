fd41e90d0d399ef55f7c0ffcbfe398fa5fe6448d
Venaxz <ivanadaniel33@gmail.com>
2026-06-03 10:58:58 +0100

    Java Output

---
+++ Output.java
@@ -0,0 +1,16 @@
+public class Output{
+	public static void main(String[] args){
+		System.out.println("Hello everyone, this is my first Java code");
+		System.out.println("Learning Java is fun");
+		System.out.print("Here I am using the print method");
+		System.out.print("This text will be appended");
+		System.out.printf("%nMercy Ben is %d years old. %n",25);
+		System.out.printf("%nMy account balance is %f%n", 7657.999);
+		System.out.printf("%nMy account balance is %,.2f%n", 76549999.922276);
+		System.out.printf("%s is a boy%n","Benjamin");
+		System.out.printf("Do you love Java? %b%n",true);
+		System.out.printf("Fuel price in Nigeria has been increased by %%80%n");
+		System.out.printf("My account balance is %c%,.2f%n",'$',76549999.922276);
+
+	}
+}
