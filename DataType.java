78270f1dca6e375d6246a8a50ea0dd885d252773
Venaxz <ivanadaniel33@gmail.com>
2026-06-04 11:52:21 +0100

    Java Keywords

---
+++ DataType.java
@@ -0,0 +1,29 @@
+public class DataType{
+	public static void main(String[] args){
+		//integer type
+		byte age = 25;
+		System.out.printf("You are %d years old%n",age );
+		
+		short quantity = 30000;
+		System.out.printf("I bought %,d quantity of phones%n",quantity);
+		
+		int population = 2025278260;
+		System.out.printf("The population of Nigeria is %,d%n",population);
+		
+		long worldPopulation = 729737380000000L;
+		System.out.printf("The world's population is %,d%n",worldPopulation);
+		
+		//Float-Point
+		float myBalance = 67060742.8672F;
+		System.out.printf("My account balance is $%,.2f%n",myBalance);
+		
+		double cbnBalance = 848930939893893893.744463;
+		System.out.printf("The CBN balnce is %,.2f%n",cbnBalance);
+		
+		char symbol = '@';
+		System.out.printf("Your email address is ivanadaniel33%cgmail.com%n",symbol);
+		
+		boolean isJava = true;
+		System.out.printf("Do you love learning Java ? %b%n",isJava);
+	}
+}
