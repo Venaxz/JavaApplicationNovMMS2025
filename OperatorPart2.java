e17ae103c0974a06e7e1d9bb590f9a1627b2aade
Venaxz <ivanadaniel33@gmail.com>
2026-06-16 11:00:12 +0100

    OperatorPar2

--- OperatorPart2.java
+++ OperatorPart2.java
@@ -4,8 +4,31 @@ public class OperatorPart2{
 		int num2 = 30;
 		int num3 = 50;
 		
-		boolean isGreater = (num1 > num2) && (num1 > num3);
+		boolean isAndLogical = (num1 > num2) && (num1 > num3);
 		
-	System.out.printf(" (%d > %d) && (%d > %d) = %b%n",num1,num2,num1,num3,isGreater);
+		boolean isOrLogical = (num1 > num2) || (num1 < num3);
+		
+		boolean isNotLogical = !((num1 > num2) || (num1 < num3));
+		
+	System.out.printf(" Is(%d > %d) && (%d > %d) = %b%n",num1,num2,num1,num3,isAndLogical);
+		
+	System.out.printf(" Is(%d > %d) || (%d < %d) = %b%n",num1,num2,num1,num3,isOrLogical);
+	
+	System.out.printf(" Is!((%d > %d) || (%d < %d)) = %b%n",num1,num2,num1,num3,isNotLogical);
+	
+	//Unary Operator(++, --)
+	//(Pre-increment , post- increment)
+	
+	int x = 10;
+	int y = 20;
+	System.out.printf("The value of x is %d%n",++x);
+	System.out.printf("The value of y is %d%n",y++);
+	System.out.printf("The current value of y is %d%n",y);
+	
+	
+	System.out.println("===========Unary Decrement===========");
+    System.out.printf("The value of x is %d%n",--x);
+	System.out.printf("The value of y is %d%n",y--);
+	System.out.printf("The current value of y is %d%n",y);
 	}
 }
