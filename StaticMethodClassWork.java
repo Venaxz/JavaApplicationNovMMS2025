717c367a72279aaba8d0b9c135fbf264d422af29
Venaxz <ivanadaniel33@gmail.com>
2026-07-02 14:10:04 +0100

    StaticMethodClassWork

---
+++ StaticMethodClassWork.java
@@ -0,0 +1,10 @@
+public class StaticMethodClassWork{
+
+    public static void main(String[] args) {
+        System.out.println(studentName("Ivana"));
+    }
+
+    public static String studentName(String name) {
+        return name;
+    }
+}
