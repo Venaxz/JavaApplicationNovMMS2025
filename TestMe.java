9e7e7c6a18656865392dd3696219159eb5ab4006
Venaxz <ivanadaniel33@gmail.com>
2026-07-02 13:52:01 +0100

    TestMe

--- TestMe.java
+++ TestMe.java
@@ -1,6 +1,8 @@
 public class TestMe{
 	public static void main(String[] args){
+		InstanceMethod test = new InstanceMethod();
 		
+		System.out.println(test.addNumber(30,90,40));
 		System.out.println(StaticMethod.addNumber(90,60,40));
 	}
 }
