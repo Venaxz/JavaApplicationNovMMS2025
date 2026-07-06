8d2736d64f1cab5f181d413ec67fba103946d09b
Venaxz <ivanadaniel33@gmail.com>
2026-07-06 13:30:35 +0100

    LocalVariable

---
+++ LocalVariable.java
@@ -0,0 +1,12 @@
+public class LocalVariable{
+	public static void main(String[] args){
+		int age = 25;
+		
+		if(age >= 18){
+			int myAge = 30;
+			System.out.printf("Miss Mercy is %d years old%n",myAge);	
+		}
+		
+		System.out.printf("Your age is %d%n",age);
+	}
+}
