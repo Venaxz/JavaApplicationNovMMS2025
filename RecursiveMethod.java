67c3af07eac438223a7ac37e303749260e58c0fc
Venaxz <ivanadaniel33@gmail.com>
2026-07-06 13:28:28 +0100

    RecursiveMethod

---
+++ RecursiveMethod.java
@@ -0,0 +1,11 @@
+public class RecursiveMethod{
+	public static void main(String[] args){
+		displayName("John Williams");
+	}
+	
+	public static void displayName(String name){
+		System.out.println(name);
+		
+		displayName(name);
+	}
+}
