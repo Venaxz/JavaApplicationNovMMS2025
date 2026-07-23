0dc66e9dab2bca2a0514204232ebb2e163c6e35a
Venaxz <ivanadaniel33@gmail.com>
2026-07-23 13:04:23 +0100

    UsingMyEnumClassWork

---
+++ UsingMyEnumClassWork.java
@@ -0,0 +1,13 @@
+Public class UsingMyEnumClassWork{
+	public static void main(String [] args){
+		System.out.println(Grade grade1 = Grade.A +"Outsatnding");
+		System.out.println(Grade grade2 = Grade.B +" Excellent");
+	    System.out.println(Grade grade3 = Grade.c +" Very Good");
+		System.out.println(Grade grade4 = Grade.D+" ";
+		System.out.println(Grade grade5 = Grade.E+" ";
+		System.out.println(Grade grade6 = Grade.F+" ";
+
+		
+		System.out.printf("Your grade is %s%n",A);
+	}
+}
